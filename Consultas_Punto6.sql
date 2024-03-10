319164ed941a08111dff4c6b166387c3cb18f560
SDM30 <151746540+SDM30@users.noreply.github.com>
2024-03-09 22:02:03 -0500

    Delete Consultas_Punto6.sql

--- Consultas_Punto6.sql
+++
@@ -1,7 +0,0 @@
-
-(Select nombre
-From(Select tomador.nombre, count(poliza.idtipocubrimiento) as numdeCubrimientos
-     From tomador
-     Join poliza On tomador.id = poliza.idtomador
-     group by tomador.nombre) t1
-Where t1.numdecubrimientos = (Select count(id) From tipo_cubrimiento));
