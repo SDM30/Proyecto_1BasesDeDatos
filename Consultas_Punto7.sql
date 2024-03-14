faf70313fd6c8fd36f80cf27dfc1a6fe2cf095b5
SDM30 <151746540+SDM30@users.noreply.github.com>
2024-03-14 08:55:28 -0500

    Update Consultas_Punto7.sql

--- Consultas_Punto7.sql
+++ Consultas_Punto7.sql
@@ -1,7 +1,7 @@
 Create view punto7 as
 (Select d.nombre as nom_departamento,
-    sum(case when t.genero = 'femenino' then 1 else 0 end) as cant_mujeres,
-    sum(case when t.genero = 'masculino' then 1 else 0 end) as cant_hombres,
+    sum(case when lower(t.genero) = 'femenino' then 1 else 0 end) as cant_mujeres,
+    sum(case when lower(t.genero) = 'masculino' then 1 else 0 end) as cant_hombres,
     count(*) as total
 from departamento d join tomador t on d.id = t.iddepartamento
 group by d.nombre);
